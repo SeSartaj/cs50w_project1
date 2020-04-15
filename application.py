44b10d0239b8f62e4771fce65c8e0b239273b8d7
sesartaj <sartajahmad1529@gmail.com>
2020-04-15 17:29:13 +0430

    All Requrements completed

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
-import os, hashlib, random
+import os, hashlib, random, requests, json
 
-from flask import Flask, session
+from flask import Flask, session, redirect, jsonify
 from flask import render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -23,18 +23,37 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-# Temporary data
-users = {"ahmad":"123"}
-
 
 @app.route("/")
 def index():
-    ## if login, continue
+    # if login, continue
     if session.get("is_login") is None:
         session["is_login"] = False
     if session.get("is_login") is True:
         return render_template("index.html", is_login=session["is_login"])
     return render_template("login.html")
+
+
+@app.route("/api/<string:isbn>")
+def book_api(isbn):
+    book = db.execute("select tb_book.title, tb_book.author, tb_book.year, tb_book.isbn, count(tb_review.review_id) as review_count, avg(tb_review.rating) as average_score from tb_book   left join tb_review on tb_book.id = tb_review.book_id where isbn = :isbn group by title, author, year, isbn", {"isbn":isbn}).fetchone()
+
+    if book is None:
+        return jsonify({"error" : "Invalid flight_id"}), 422
+
+    if book.average_score is  None:
+        book.average_score = 0
+
+    return jsonify({
+        "title": book.title,
+        "author": book.author,
+        "year": book.year,
+        "isbn": book.isbn,
+        "review_count": book.review_count,
+        "average_score": float(book.average_score)
+    })
+
+
 
 
 @app.route("/login")
@@ -53,11 +72,11 @@ def login():
 
     # Compare Name and Password with Database
     # make sure user exist
-    user =  db.execute("Select * from tb_user where username = :name", {"name":name}).fetchone()
-    if user is None:
+    session["user"] =  db.execute("Select * from tb_user where username = :name", {"name":name}).fetchone()
+    if session["user"] is None:
         return "User does not exist"
     # make sure password is correct
-    if user.password != hash_password:
+    if session["user"].password != hash_password:
         return "password is wrong"
     session["is_login"] = True
     return render_template("index.html", is_login=session["is_login"])
@@ -80,35 +99,56 @@ def register():
     try:
         db.execute("insert into tb_user (username, password) values (:username, :password)", {"username":name, "password":hash_password})
         db.commit()
-        session["is_login"] = True
-        return render_template("index.html")
+        return render_template("login.html")
     except RuntimeError:
         return "There was a problem, please try again"
 
-@app.route("/book/<string:isbn>", methods=["GET"])
+@app.route("/book/<string:isbn>", methods=["GET", "POST"])
 def book(isbn):
-    book = db.execute("select * from tb_book where isbn = :isbn", {"isbn":isbn}).fetchone()
 
-    if book is None:
-        return render_template("error.html", message="The book you are looking is not found")
-    reviews = ["Very nice book", "must read bok", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
-    return render_template("book.html", book=book, reviews=reviews, is_login=session["is_login"])
-@app.route("/book/<string:isbn>", methods=["POST"])
-def rating(isbn):
-    session["rating5"] = request.form.get("rating5")
-    session["rating_text"] = request.form.get("rating_text")
+    if request.method == "GET":
+        session["book"] = db.execute("select * from tb_book where isbn = :isbn", {"isbn":isbn}).fetchone()
 
-    if session["is_login"] is False:
-        return render_template("error.html", text="In order to make reviews, you must login first. Click <a href='{ url_for(\"login\") }}'>here</a> to login")
+        if session["book"] is None:
+            return render_template("error.html", message="The book you are looking is not found")
 
-    db.execute("insert into tb_rating (book_id, user_id, rating, review_text) values ")
+        reviews = db.execute("select tb_review.rating, tb_review.review_text, tb_user.username from tb_review inner join tb_user on tb_review.user_id = tb_user.id where tb_review.book_id = :book_id", {"book_id":session["book"].id})
+        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key" : os.getenv("grKey"), "isbns" : isbn})
+        if res.status_code != 200:
+            return render_template("error.html", text= "API does not work. {}".format(res.status_code))
+        res = res.json()
+        average_rating = res["books"][0]["average_rating"]
+        ratings_count = res["books"][0]["ratings_count"]
 
+        return render_template("book.html", book=session["book"], reviews=reviews, is_login=session["is_login"], average_rating = average_rating, ratings_count=ratings_count)
+
+    elif request.method == "POST":
+        session["rating5"] = request.form.get("rating5")
+        session["rating_text"] = request.form.get("rating_text")
+
+        if session["is_login"] is False:
+            return render_template("error.html", text="In order to make reviews, you must login first.")
+
+        # One review per user
+        review_count = db.execute("select count(*) from tb_review where user_id = :user_id and book_id = :book_id", {"user_id":session["user"].id, "book_id":session["book"].id}).fetchone()[0]
+        if review_count > 0:
+            return render_template("error.html", text = "Sorry, One user cannot submit more than one review. ")
+        # try:
+        #     db.execute("UPDATE table tb_review set rating = :rating, review_text = :review_text where book_id = :book_id and user_id = :user_id;", {"rating":session["rating5"], "review_text":session["rating_text"], "book_id":session["book"].id, "user_id" = session["user"].id})
+        #     return redirect("/bok/{}".format(isbn))
+        # except:
+        try:
+            db.execute("insert into tb_review (book_id, user_id, rating, review_text) values(:book_id, :user_id, :rating, :review_text)", {"book_id":session["book"].id, "user_id":session["user"].id, "rating":session["rating5"], "review_text":session["rating_text"]})
+            db.commit()
+            return redirect('/book/'+ isbn)
+        except RuntimeError:
+            return render_template("error.html", text="We couldn't save your review. Try again later");
 
 
 @app.route("/Logout")
 def logout():
     session["is_login"] = False
-    return render_template("login.html")
+    return redirect("/login")
 
 
 @app.route("/search", methods=["POST"])
